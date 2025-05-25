327c8ca20d5194bfd1bf9c93dfefb570d879550f
Chandan <chandan.24bcs10105@sst.scaler.com>
2025-05-25 14:41:42 +0530

    Changed model address

--- backend/sanskrit_app/models/yolo/yolo_ocr.py
+++ backend/sanskrit_app/models/yolo/yolo_ocr.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
 def yolo_model(model_path="sanskrit_app/models/yolo/best.pt"):
-    model = YOLO("/home/neuralsynth/Code/Projects/sanskrit-ocr/backend/sanskrit_app/models/yolo/best.pt")
+    model = YOLO("./best.pt")
     return model
