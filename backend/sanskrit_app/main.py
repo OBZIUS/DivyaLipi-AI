14c39f5f7a912435f4862c0843552443f5e84fda
Chandan P <95340276+NoiceHax@users.noreply.github.com>
2025-05-25 14:06:05 +0530

    Update main.py

--- backend/sanskrit_app/main.py
+++ backend/sanskrit_app/main.py
@@ -5,10 +5,13 @@ from fastapi.responses import JSONResponse
 import logging
 
 app = FastAPI()
-
+origins = [
+    "https://divya-lipi-ai.vercel.app/",  
+    "http://localhost:8888" 
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
